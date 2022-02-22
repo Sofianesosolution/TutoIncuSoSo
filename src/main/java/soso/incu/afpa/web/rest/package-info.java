404b5b763b0ba6fb8eef8403c730dce444b36d72
westsidecz <westsidecz@gmail.com>
2022-02-22 14:48:25 +0100

    Initial version of demo generated by generator-jhipster@7.6.0

---
+++ src/main/java/soso/incu/afpa/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package soso.incu.afpa.web.rest;
---
+++ src/main/java/soso/incu/afpa/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package soso.incu.afpa.web.rest.vm;
